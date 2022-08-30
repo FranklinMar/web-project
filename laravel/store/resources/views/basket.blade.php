a853cd67928008389fff0e48fc47894e04e001df
denisbereznuuk <denisbereznuuk@gmail.com>
2022-08-30 18:32:08 +0300

    Basket HTML/CSS templates

--- laravel/store/resources/views/basket.blade.php
+++ laravel/store/resources/views/basket.blade.php
@@ -149,7 +149,7 @@
       </button>
     </span>
   </nav>
-  <main style="display: flex; flex-direction: column; padding-top: 50px">
+  <main style="padding-top: 50px; align-items:center;">
     <div class="menu">
       <div class="menu-panel">
         <div class="flex-row">
@@ -195,199 +195,107 @@
         <!--                </div>-->
       </div>
     </div>
-    @isset ($games)
-    <div class="basket basket-group layout">
-      <div class="basket-flex layout">
-        <div class="basket-flex15 layout">
-          <div class="basket-txt12 layout">ВАШ КОШИК.</div>
-          <div class="basket-txt12 layout">Гаманець: ${{ number_format((double)$customer->money, 2, '.', '') }}</div>
-          @isset($error)
-          <div class="error" style="display:contents;"><a>Помилка: {{ $error }}</a></div>
-          @endisset
-          <!--            <div style="&#45;&#45;src:url(//img/line.png)" class="basket-img layout8"></div>-->
-          <img src="/img/line.png" class="basket-img layout8" alt="">
-        </div>
-
-
-
-        <!-- <div class="basket-group6 layout" style="flex-direction: row; justify-content: space-between;"> -->
-        <!-- <div class="basket-flex16 layout" style="width:100%;"> -->
-        <!-- <div class="basket-flex16-item" style="width:100%;"> max-width: 180px; -->
-        <!--                    <div style="&#45;&#45;src:url(//img/gtaposter.png)" class="basket-img3 layout"></div>-->
-        <!-- <div class="basket-group6 layout" style="flex-direction: row; justify-content: space-between;">
-            <div class="basket-flex16 layout" style="width:100%;">
-                <div class="basket-flex16-item" style="width:100%;">
-                    <img src="/img/gtaposter.png" class="basket-img3 layout" alt="Постер">
-                </div>
-                <div class="basket-flex16-spacer"></div>
-                <div class="basket-flex16-item1" style="display: contents; width:100%; flex: none;">
-                    <div class="basket-flex17 layout" style="width: 100%;">
-                        <div class="basket-flex18 layout" style="justify-content: space-between;">
-                            <div class="basket-flex18-item" style="flex: none;">
-                                <div class="basket-flex19 layout">
-                                    <div class="basket-txt13 layout">Grand theft auto V: Premium Edition</div>
-                                    <div class="basket-img layout9" style="max-height: 32px;max-width: 120px;
-height:100%;width:100%">
-                                        <img src="/img/epic.svg" alt="Epic Games Store" style="height: 32px;width: 30px;">
-                                        <img src="/img/steam.svg" alt="Steam" style="height: 32px;width: 30px;">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="basket-flex18-spacer"></div>
-                            <div class="basket-txt14 layout">$25.00</div>
-                        </div>
-                        <div class="basket-flex20 layout" style="margin: 7px 0 0 0;justify-content: right; width: auto;">
-                            <form style="display:contents;" action="" method="POST">
-                            <button style="display:contents;" type="submit">
-                            <div class="basket-flex20-item button">
-                                <div class="basket-group7 layout"><div class="basket-txt15 layout">Придбати зараз</div></div>
-                            </div>
-                            </button>
-                            </form>
-                            <div class="basket-flex20-spacer"></div>
-                            <form style="display:contents;" action="" method="POST">
-                            <button style="display: contents;" type="submit">
-                            <div class="basket-flex20-item1 button">
-                                <div class="basket-group8 layout"><div class="basket-txt16 layout">Вилучити</div></div>
-                            </div>
-                            </button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div> -->
-        @foreach ($games as $game)
-        <div class="basket-group6 layout" style="flex-direction: row; justify-content: space-between;">
-          <div class="basket-flex16 layout" style="width:100%;">
-            <div class="basket-flex16-item" style="width:100%;">
-              <img src="/games/{{ $game->url }}" class="basket-img3 layout" alt="Постер">
-            </div>
-            <div class="basket-flex16-spacer"></div>
-            <div class="basket-flex16-item1" style="display: contents; width:100%; flex: none;">
-              <div class="basket-flex17 layout" style="width: 100%;">
-                <div class="basket-flex18 layout" style="justify-content: space-between;">
-                  <div class="basket-flex18-item" style="flex: none;">
-                    <div class="basket-flex19 layout">
-                      <div class="basket-txt13 layout">{{ $game->name }}</div>
-                      <div class="basket-img layout9" style="max-height: 32px; max-width: 120px; height:100%; width:100%">
-                        @foreach ($game->platforms as $platform)
-                        <img src="/platforms/{{ $platform->url }}" alt="{{ $platform->name }}" style="height: 32px;width: 30px;">
-                        @endforeach
-                      </div>
-                    </div>
+    <div class="main">
+      <div class="header">
+        <h1>Ваш кошик</h1>
+        <div class="divider"></div>
+        <h2>Ваш гаманець: ${{ number_format((double)$customer->money, 2, '.', '') }}</h2>
+        @isset($error)
+        <div class="error" style="display:contents;"><a>{{ $error }}</a></div><!--Помилка: -->
+        @endisset
+      </div>
+      <div class="content">
+        <div class="games">
+          @foreach ($games as $game)
+          <div class="game">
+            <img src="/games/{{ $game->url }}" alt="">
+            <div class="column">
+              <div class="row space">
+                <div class="info">
+                  <h3>{{ $game->name }}</h3>
+                  <div class="platforms">
+                    @foreach ($game->platforms as $platform)
+                    <img src="/platforms/{{ $platform->url }}" alt="{{ $platform->name }}">
+                    @endforeach
                   </div>
-                  <div class="basket-flex18-spacer"></div>
-                  <div class="basket-txt14 layout">${{ number_format((double)($game->price - ($game->price * $game->discount/100)), 2, '.', '') }}</div>
                 </div>
-                <div class="basket-flex20 layout" style="margin: 7px 0 0 0;justify-content: right; width: auto;">
-                  <form style="display:contents;" action="/basket/buy" method="POST">
-                    @csrf
-                    <input type="hidden" name="idGame" value="{{ $game->id }}">
-                    <button style="display:contents;" type="submit">
-                      <div class="basket-flex20-item button">
-                        <div class="basket-group7 layout">
-                          <div class="basket-txt15 layout">Придбати зараз</div>
-                        </div>
-                      </div>
-                    </button>
-                  </form>
-                  <div class="basket-flex20-spacer"></div>
-                  <form style="display:contents;" action="/basket/delete" method="POST">
-                    @csrf
-                    <input type="hidden" name="idGame" value="{{ $game->id }}">
-                    <button style="display: contents;" type="submit">
-                      <div class="basket-flex20-item1 button">
-                        <div class="basket-group8 layout">
-                          <div class="basket-txt16 layout">Вилучити</div>
-                        </div>
-                      </div>
-                    </button>
-                  </form>
-                </div>
+                <h4>${{ number_format((double)($game->price - ($game->price * $game->discount/100)), 2, '.', '') }}</h4>
               </div>
-            </div>
-          </div>
-        </div>
-        @endforeach
-
-
-        <div class="basket-group9 layout">
-          <div class="basket-flex21 layout" style="align-content: flex-end;">
-            <div class="basket-flex22 layout" style="flex-direction: row">
-              <div class="basket-flex22-item">
-                <div class="basket-txt layout1">Підсумок:</div>
-              </div>
-              <div class="basket-flex22-spacer"></div>
-              <div class="basket-flex22-item">
-                <div class="basket-txt17 layout">
-                  {{ ($total != 0) ? '$'.number_format((double)$total, 2, '.', '') : 'Пусто' }}
-                </div>
-              </div>
-            </div>
-            @if (count($games) != 0)
-            <form action="/basket/buyall" style="display: contents" method="POST">
-              <div class="basket-group10 layout button" style="margin: 0; align-self: flex-end;">
+              <form class="row end">
                 @csrf
-                <input type="hidden" name="idCustomer" value="{{ $customer->id }}">
-                <button type="submit" style="display: contents">
-                  <div class="basket-txt18 layout">Придбати</div>
+                <input type="hidden" name="idGame" value="{{ $game->id }}">
+                <button class="button green" formaction="/basket/buy" formmethod="POST">
+                  Придбати зараз
                 </button>
-              </div>
-            </form>
-            @endif
-          </div>
-        </div>
-        <div class="basket-flex23 layout">
-          <a href="/shop" class="basket-flex23-item button">
-            <div class="basket-group11 layout">
-              <div class="basket-txt19 layout">Продовжити придбання</div>
+                <button class="button purple" formaction="/basket/delete" formmethod="POST">
+                  Вилучити
+                </button>
+              </form>
             </div>
+          </div>
+          <div class="small-game">
+            <div class="info">
+              <img src="/games/{{ $game->url }}" alt="">
+              <div class="column">
+                <h3>{{ $game->name }}</h3>
+                <div class="row">
+                  <h4>$25.00</h4>
+                  <div class="platforms">
+                    @foreach ($game->platforms as $platform)
+                    <img src="/platforms/{{ $platform->url }}" alt="{{ $platform->name }}">
+                    @endforeach
+                  </div>
+                </div>
+              </div>
+            </div>
+            <form class="buttons">
+              @csrf
+              <input type="hidden" name="idGame" value="{{ $game->id }}" style="display: none;">
+              <button class="button green" formaction="/basket/buy" formmethod="POST">
+                Придбати зараз
+              </button>
+              <button class="button purple" formaction="/basket/delete" formmethod="POST">
+                Вилучити
+              </button>
+            </form>
+          </div>
+          @endforeach
+        </div>
+        <div class="total">
+          <!-- <div class="row"> -->
+            <h3>Підсумок:</h3>
+            <h4>{{ ($total != 0) ? '$'.number_format((double)$total, 2, '.', '') : 'Пусто' }}</h4>
+          <!-- </div> -->
+
+        </div>
+        <div class="options">
+          <a href="/shop" class="button blue">
+            Продовжити придбання
           </a>
-          <div class="basket-flex23-spacer"></div>
-          @if (count($games) != 0)
-          <form style="display:contents;" action="/basket/deleteall" method="POST">
+          <form class="row">
             @csrf
             <input type="hidden" name="idCustomer" value="{{ $customer->id }}">
-            <div class="basket-flex23-item1 button">
-              <button type="submit" style="display:contents;">
-                <div class="basket-group12 layout">
-                  <div class="basket-txt20 layout">Вилучити все</div>
-                </div>
-              </button>
-            </div>
+            @if (count($games) != 0)
+            <button class="button green" formaction="/basket/buyall" formmethod="POST">Придбати</button>
+            <button class="button purple" formaction="/basket/deleteall" form`method="POST">Вилучити все</button>
+            @endif
           </form>
-          @endif
         </div>
-        <div class="basket-txt layout2">ДОСТАВКА</div>
-        <div class="basket-group13 layout">
-          <div class="basket-flex24 layout">
-            <div class="basket-flex24-item">
-              <div class="basket-group14 layout">
-                <!--                        <div style="&#45;&#45;src:url(//img/4.png)" &lt;!&ndash;class="basket-img layout10"&ndash;&gt;></div>-->
-                <div class="basket-img layout10">
-                  <img src="/img/deliver.svg" style="object-fit: contain; margin: auto" alt="Доставка">
-                </div>
-              </div>
+        <div class="delivery">
+          <h3>ДОСТАВКА</h3>
+          <div class="body">
+            <div class="image">
+              <img src="/img/deliver.svg" alt="">
             </div>
-            <div class="basket-flex24-spacer"></div>
-            <div class="basket-flex24-item1">
-              <div class="basket-flex25 layout">
-                <div class="basket-txt21 layout">
-                  Усі цифрові товари доставляються через цифрові ключі на зазначених платформах.
-                </div>
-                <div class="basket-txt22 layout">
-                  Ваші ігри будуть доступні для використання після активації цифрового ключа на платформах партнерів
-                  магазину (Steam/EpicGames/GOG/...) в кінці процесу придбання.
-                </div>
-              </div>
+            <div class="column">
+              <p>Усі цифрові товари доставляються через цифрові ключі на зазначених платформах.</p>
+              <small>Ваші ігри будуть доступні для використання після активації цифрового ключа на платформах партнерів магазину (Steam/EpicGames/GOG/...) в кінці процесу придбання.</small>
             </div>
           </div>
         </div>
       </div>
-
-
-      <footer>
+    </div>
+    <footer>
         <div class="left">
           <div class="logo">
             <img src="/img/logotype.png" class="logo-img" alt="Galactic games">
@@ -409,18 +317,7 @@ height:100%;width:100%">
           </div>
         </div>
       </footer>
-    </div>
-    @else
-    <div class="error">
-      <h1> Error 404.</h1><br>
-      <p>Sorry but there is no game here you are looking for.</p>
-      <a href="/shop"> Return to shop </a>
-    </div>
-    @endisset
   </main>
-  <script type="text/javascript">
-    AOS.init();
-  </script>
 </body>
 
 </html>
